08e38a43c1ac3b44d3fd203051d57dbcaafcf911
emacliam <mushyp1608@gmail.com>
2020-08-06 14:12:55 +0200

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,9 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        bingo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+ <main>
+   <h2>
+       My nuxt app
+   </h2>
+ </main>
 </template>
 
 <script>
