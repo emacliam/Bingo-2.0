c0cf72e31c51c4a5c543b1f8f07e71e0ca57e063
emacliam <mushyp1608@gmail.com>
2020-08-06 15:26:08 +0200

    first commit

---
+++ components/mobile/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+      <input type="search" name="" id="" class="w-full h-8 bg-white rounded-lg border pl-2">
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
