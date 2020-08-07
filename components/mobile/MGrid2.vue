b35acbd60fc3437e22242ed962d642c0564f86fb
emacliam <mushyp1608@gmail.com>
2020-08-07 11:45:23 +0200

    added drawer on mobile layout

---
+++ components/mobile/MGrid2.vue
@@ -0,0 +1,145 @@
+<template>
+<main class="">
+<!-- component -->
+<!-- post card -->
+<div class="flex text-sm card"><!--horizantil margin is just for display-->
+  <div class="bg-white shadow-lg rounded-lg mx-4 md:mx-auto max-w-md md:max-w-2xl px-4 flex-1 py-2">
+        <span class="font-bold">Recommended by Bingo:</span>
+          <div class="my-2">
+            <hr>
+        </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Innscor Group</span>
+            </div>
+            <div>
+               <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+            <div class="ml-12">
+                <hr>
+            </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Econet Wireless</span>
+            </div>
+            <div>
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px"  viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+        <div class="ml-12">
+                <hr>
+            </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">TechZim</span>
+            </div>
+            <div>
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+    </div>
+   </div>
+
+
+<div class="flex mt-2 text-sm card">
+    <div class="bg-white shadow-lg rounded-lg mx-4 md:mx-auto max-w-md md:max-w-2xl px-4 flex-1 py-2">
+        <span class="font-bold">Trending:</span>
+          <div class="my-2">
+            <hr>
+        </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Techzim</span>
+            </div>
+            <div>
+               <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+        <div class="ml-12">
+                <hr>
+            </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Zimbocash</span>
+            </div>
+            <div>
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px"  viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+        <div class="ml-12">
+                <hr>
+            </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Bingo</span>
+            </div>
+            <div>
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+ <div class="flex mt-2 text-sm card">
+    <div class="bg-white shadow-lg rounded-lg mx-4 md:mx-auto max-w-md md:max-w-2xl px-4 flex-1 py-2">
+        <span class="font-bold">Suggested for you:</span>
+        <div class="my-2">
+            <hr>
+        </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Privor</span>
+            </div>
+            <div>
+               <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+        <div class="ml-12">
+                <hr>
+            </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Okay zw</span>
+            </div>
+            <div>
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px"  viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+        <div class="ml-12">
+                <hr>
+            </div>
+        <div class="flex items-center m-1 justify-between">
+            <div class="flex items-center">
+            <img src="" alt="" class="rounded-full w-8 h-8 bg-gray-500">
+            <span class="ml-4 font-bold">Steward Bank</span>
+            </div>
+            <div>
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><path d="M384 265H264v119h-17V265H128v-17h119V128h17v120h120v17z"/><metadata><rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:dc="http://purl.org/dc/elements/1.1/"><rdf:Description about="https://iconscout.com/legal#licenses" dc:title="ios,plus,empty" dc:description="ios,plus,empty" dc:publisher="Iconscout" dc:date="2017-09-24" dc:format="image/svg+xml" dc:language="en"><dc:creator><rdf:Bag><rdf:li>Benjamin J Sperry</rdf:li></rdf:Bag></dc:creator></rdf:Description></rdf:RDF></metadata></svg>
+            </div>
+        </div>
+    </div>
+  </div>
+</main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.card{
+    width:99%;
+}
+</style>
