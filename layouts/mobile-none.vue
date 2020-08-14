b559e6f8bbb75a6fc6fd43e92ccba4cfa9a8e3ec
emacliam <mushyp1608@gmail.com>
2020-08-14 17:57:16 +0200

    fixing mobile login

---
+++ layouts/mobile-none.vue
@@ -0,0 +1,26 @@
+<template>
+<main class="max-w-full max-h-full">
+    <nav class="bg-blue-700 font-bold pl-4 h-12 flex items-center">
+     Bingo
+    </nav>
+    <div>
+        <nuxt/>
+    </div>
+
+</main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+body{
+    --bg-opacity: 1;
+    background-color: #edf2f7;
+    background-color: rgba(237, 242, 247, var(--bg-opacity));
+}
+
+</style>
--- layouts/mobile.vue
+++ layouts/mobile.vue
@@ -2,7 +2,7 @@
   <main class="">
 <nav
     :class="{ 'scrolled': !view.atTopOfPage }"
-    class=" tp flex fixed w-full items-center justify-between flex-wrap px-6 h-12 bg-blue-600  z-10 top-0 animated">
+    class=" tp flex fixed w-full items-center justify-between flex-wrap px-6 h-12 bg-blue-700  z-10 top-0 animated">
 
       <div class="flex items-center justify-between">
         <div class="flex items-center text-sm">
@@ -10,7 +10,7 @@
         </div>
       </div>
       <div class="flex items-center text-white">
-        <nuxt-link to="/signin" class="m-2 hover:bg-blue-300 p-1 rounded-lg text-sm">Sign In</nuxt-link>
+        <nuxt-link to="/mobile/Login" class="m-2 hover:bg-blue-300 p-1 rounded-lg text-sm">Sign In</nuxt-link>
         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 26 26"><path style="line-height:normal;text-indent:0;text-align:start;text-decoration-line:none;text-transform:none;block-progression:tb" d="M 13 0 C 5.826231 0 1.6e-005 5.8262 0 13 C -4.2e-005 20.1738 5.826191 26 13 26 C 20.173809 26 26.000041 20.1738 26 13 C 25.999984 5.8262 20.173769 0 13 0 z M 13 1 C 19.633359 1 24.999985 6.3666 25 13 C 25.000038 19.6334 19.633396 25 13 25 C 6.366603 25 0.999962 19.6334 1 13 C 1.000015 6.3666 6.366641 1 13 1 z M 7 7 L 7 8 L 8.5039062 8 L 10.867188 15.246094 L 9.9765625 18.367188 C 9.8924425 18.663257 10.149243 19.003576 10.457031 19.003906 L 18.457031 19.003906 L 18.457031 18.003906 L 11.119141 18.003906 L 11.6875 16.009766 L 17.830078 16 L 19.947266 10.158203 L 19.007812 9.8164062 L 18.574219 11.015625 L 10.539062 11.015625 L 9.2285156 7 L 7 7 z "  font-family="sans-serif" font-weight="400" overflow="visible"/></svg>
       </div>
 </nav>
